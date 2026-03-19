2ec840d603ca9e3acc3f7ab2482e755891d54c9c
Hackerlord1 <hemankipkoechchirchir@gmail.com>
2026-03-19 21:49:56 +0300

    fix: remove basePath for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  output: "export",
-  basePath: "/phone-locator-pro",
-  assetPrefix: "/phone-locator-pro/",
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 export default nextConfig;
